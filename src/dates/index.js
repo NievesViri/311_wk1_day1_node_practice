77143d109f5fc9d25ec773ea769d13d2226cbfdd
NievesViri <105406390+NievesViri@users.noreply.github.com>
2023-01-05 00:28:08 -0600

    6 passing

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,22 +1,21 @@
 // import moment here; use this package in each function
+<script src="moment.js"></script>;
 
 const today = () => {
   // write code for dates.today
-
-}
+  return day.moment().format("dddd");
+};
 
 const calendar = () => {
   // write code for dates.calendar
-
-}
+};
 
 const currentTime = () => {
   // write code for dates.currentTime
-
-}
+};
 
 module.exports = {
   today,
   calendar,
-  currentTime
-}
+  currentTime,
+};
